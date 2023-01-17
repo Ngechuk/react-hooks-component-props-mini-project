78decf90c9c93ed280c16525bf36cf5b1b8f6f95
Kevin <kelvinngechu@gmail.com>
2023-01-17 20:12:08 +0300

    about

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,19 @@
 import React  from "react";
-
-function Article() {
-    const {title, date = "January 1, 1970", preview } = props;
+export default function Article(
+  {
+    title, 
+    date = "January 1, 1970", 
+    preview, 
+    minutes
+  }
+) {
   return (
-    <article>
-    <h3>{title}</h3>
-    <small>{date}</small>
-    <p>{preview}</p>
-  </article>
-  );
+    <div>
+         <article>
+            <h3>{title}</h3>
+            <small>{date}{minutes} min read</small>
+            <p>{preview}</p>
+        </article>
+    </div>
+  )
 }
-export default Article;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-function Header(props) {
+export default function (props) {
   return (
-    <header>
-      <h1>{props.blogName}</h1>
-    </header>
-  );
+    <div>
+        <header>
+            <h1>{props.name}</h1>
+        </header>
+    </div>
+  )
 }
-
-export default Header;
